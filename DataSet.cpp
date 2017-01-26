a04dec1f87b913814a7cb7aac5c0effa1388d29d
halidziya <halidziya@gmail.com>
2017-01-26 13:49:32 -0500

    Default Psi depends m

--- DataSet.cpp
+++ DataSet.cpp
@@ -7,7 +7,7 @@ int hypersample = 1;
 DataSet::DataSet(int argc,char** argv)
 {
 	Matrix conf;
-	if ((argc > 1) & fexists(argv[1])) {
+	if ((argc > 1) &&  fexists(argv[1])) {
 		data.readBin(argv[1]); // Datafile
 	}
 	else {
@@ -17,7 +17,27 @@ DataSet::DataSet(int argc,char** argv)
 	n = data.r;
 	d = data.m;
 	init_buffer(nthd, d);
-	if ((argc > 2) & fexists(argv[2]))
+
+	if ((argc > 3) && fexists(argv[3]))
+	{
+		conf.readBin(argv[3]);
+		m = conf(0)[1];
+		kappa = conf(0)[2];
+		kappa1 = conf(0)[3];
+		alpha = conf(0)[4];
+		gam = conf(0)[5];
+	}
+	else
+	{
+		m = d + 2;
+		kappa = 0.05;
+		kappa1 = 0.5;
+		alpha = 0.1;
+		gam = 1;
+	}
+
+
+	if ((argc > 2) && fexists(argv[2]))
 	{
 		prior.readBin(argv[2]);
 		hypersample = 1; // 0 actual value would be changed in further versions
@@ -28,30 +48,13 @@ DataSet::DataSet(int argc,char** argv)
 	}
 	else
 	{
-		Psi = eye(d)*(m - d - 1)*2;
-		mu0 = data.mean();
+		Psi = (eye(d)*(m - d - 1)).copy();
+		mu0 = data.mean().copy();
 	}
 	
-	if ((argc > 3) & fexists(argv[3]))
-	{
-		conf.readBin(argv[3]);
-		m = conf(0)[1];
-		kappa = conf(0)[2];
-		kappa1 = conf(0)[3];
-		alpha = conf(0)[4];
-		gam = conf(0)[5];
-		cout << kappa << " " << kappa1 << " " << alpha << " " << gam;
-	}
-	else
-	{
-		m = d + 2;
-		kappa = 0.05;
-		kappa1 = 0.5;
-		alpha = 1;
-		gam = 1;
-	}
+
 	
-	
+	cout << n << " " << d << " " << kappa << " " << kappa1 << " " << alpha << " " << gam << endl;
 
 	//if ((argc > 2) & fexists(argv[2]))
 		//group.readBin(groupfile);
